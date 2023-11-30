340514c4219e6d5c25c763ccc1bb084826360bf7
Lucas Azevedo <lucas.a.souza@hotmail.com>
2023-11-30 10:22:32 -0300

    Documentação

--- AutoClickerdocuSign.py
+++ AutoClickerdocuSign.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 
 time.sleep(5)
-
+## Função para mover o mouse para uma posição específica
 def moveMouse(x, y):
     pyautogui.moveTo(x, y)
 
@@ -26,18 +26,19 @@ total_repetitions = 520
 # Loop principal
 for i in range(total_repetitions):
     
-
-    # Alternar entre -240 e -235 a cada iteração
+    # Alternar entre -240 e -235 a cada iteração (mudando o valor de scroll_value a cada iteração entre número impares e pares)
     if i % 2 == 0:
         scroll_values = [-240, -230]
     else:
         scroll_values = [-230, -240]
-
+    # Loop para rolar a página para baixo e para cima
     for scroll_value in scroll_values:
         # Chamar clickBaixar a cada rolagem
         clickBaixar()
 
+        # Rolar a página para baixo ou para cima
         pyautogui.scroll(scroll_value)
+
         # Agora, mova o mouse para uma posição fixa após cada rolagem
         moveMouse(1760, 421)
         time.sleep(2)
